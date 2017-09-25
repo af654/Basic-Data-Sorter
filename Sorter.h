61d72426910c038fbdd29ab9d5582068b81d97b7
Brooks Tawil <tawilbrooks@gmail.com>
2017-09-25 18:05:04 -0400

    Add command line argument check and gitignore

--- Sorter.h
+++ Sorter.h
@@ -38,8 +38,41 @@ const char* validColums[] = {
     "aspect_ratio",
     "movie_facebook_likes"};
 
+    //Suggestion: define a struct that mirrors a record (row) of the data set
+typedef struct row
+{
+	char* color; 
+	char* director_name;
+	int num_critic_for_reviews;
+	char* duration;
+	int director_facebook_likes;
+	int actor_3_facebook_likes;
+	char* actor_2_name;
+	int actor_1_facebook_likes;
+	int gross;
+	char* genres;
+	char* actor_1_name;
+	char* movie_title; 
+	int num_voted_users;
+	int cast_total_facebook_likes;
+	char* actor_3_name;
+	int facenumber_in_poster;
+	char* plot_keywords;
+	char* movie_imdb_link;
+	int num_user_for_reviews;
+	char* language;
+	char* country;
+	char* content_rating;
+	int budget;
+	int title_year;
+	int actor_2_facebook_likes;
+	double imdb_score;
+	double aspect_ratio;
+	int movie_facebook_likes;
+};
+
 //Suggestion: define a struct that mirrors a record (row) of the data set
-struct row {
+struct brooks_row {
     //I believe reading in all values as char* gives the most flexibility as the values read in are a mix of strings, ints and doubles.
     //We can use strcmp and it should work functionally just as well for values that are true strings, and values that are just numbers.
     char* filmColor;
--- sorter.c
+++ sorter.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 #include "Sorter.h"
+#include "mergesort.c"
 
-int main(int argc, char *argv[]) {
-    
-    /*
-    Here are the steps that need to be accomlished:
-    1. Read in the command line argument for which column is relevant. Check to see if the column is valid. 
-        (We can make it return some error and stop execution, or just us a default value for sorting like movie_title.)
-    2. Read in the CSV file from the STDIN and create row structs for each row read in.
-        Remeber the first line is just the columns and should NOT be factored into a movie row.
-    3. Call mergesort using the appropriate column on this collection of rows
-    4. Once mergesort is complete, output all rows to STDOUT using fprintf
-    */
-
-
-    ///////////////////////////////////
-    //Check the command line arguments
-    ///////////////////////////////////
-
-    //Checks to see if there is only 3 arguments, filename(argc[0]), -c option, column(argc[1])
-    //Also the second argument must be -c, followed by a valid column.
-    //Delivers different messages based on the error.
-    if ( argc != 3 
-            || strcmp(argv[1],"-c") != 0) {
+int main(int argc, char* argv[]) {
+	//test case 1: make sure that if file 1 is empty it prints out 0
+	if ( argc != 3 
+        || strcmp(argv[1],"-c") != 0) {
         printf("The command line must follow the following format:\ncat input.file | ./sorter -c  movie_title");
     } else if (!isValidColumn(argv[2])) {
         printf("%s is not a valid column type. Please consult documentation for list of proper columns.",argv[2]);
     }
 
-    ///////////////////////////////////
-    //Read in the lines from CSV
-    ///////////////////////////////////
+    //name of csv file and open it
+	char* name = argv[1]; 
+    FILE* file;
+	file = fopen(name, "r");
 
-}
+	//if file does not exist 
+	if(!file){
+		printf("error\n");
+		return 1;
+	}
+
+	//creating a new csv file means making a 2D array of 28 columns and a certain amount of rows
+	//malloc for 28 "column" structs and put them into an array (each 28*1)
+	//and then for each one malloc for 1000 nodes (so now it's 28*1000)
+	//28*100 at first and then realloc if we need to -> 28*1000 etc
+	struct row *oneDarray=malloc(sizeof(struct row));
+
+	char line[28];
+	char buff[64];
+	//go through csv line by line and read into each separate linked list 
+	while(fgets(line,  buff, file)){
+
+		//end of file
+		if(feof(file)){
+			printf("%d\n", counter);
+	  		break;
+	  	}
+
+	  	//tracing through csv
+	  	//not going to be 28 nor 1000 idek how to go through it if we dont know the set number of rows
+	  	for(int column = 0; column < 28; column++) {
+	  		for(int row = 0; row < 1000; row++){
+
+	  		} //end of inner for
+	  	} //end of outer for
+
+	} //end of while
+} //end of main
 
 //const array is put in the header file, input string is compared against names found there.
-//returns 1 if the inut is a valid column that can be searched, 0 otherwise
+//returns 1 if the input  is a valid column that can be searched, 0 otherwise
 int isValidColumn(char* columnName) {
     for(int i = 0; i < (sizeof(validColums) / sizeof(validColums[i])); i++){
         if(strcmp(validColums[i], columnName) == 0) {
